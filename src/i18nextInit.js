411083906d2c2d1698064b9864a4e0ab89d59816
Daniel Duan <dduan@yahoo.com>
2020-05-26 16:24:27 -0700

    async locale

--- src/i18nextInit.js
+++ src/i18nextInit.js
@@ -1,10 +1,25 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
+import HttpApi from 'i18next-http-backend';
 
+import bundles, { availableLocales } from './localeBundles';
 import en from './locales/en.json';
-import es from './locales/es.json';
-import zh from './locales/zh.json';
+
+const DEFAULT_LOCALE = 'en';
+
+// load the right bundle depending on the requested locale
+// option to include a default locale so it's always bundled and can be used as fallback
+function loadLocaleBundle(locale) {
+  if (locale !== DEFAULT_LOCALE) {
+    return bundles[locale]()
+      .then(data => data.default) // ES6 default import
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+  return Promise.resolve(en);
+}
 
 const langDetectorOptions = {
   // order and from where user language should be detected
@@ -22,33 +37,52 @@ const langDetectorOptions = {
   checkWhitelist: true,
 };
 
+const backendOptions = {
+  loadPath: '{{lng}}|{{ns}}', // used to pass language and namespace to custom XHR function
+  request: (options, url, payload, callback) => {
+    // instead of loading from a URL like i18next-http-backend is intended for, we repurpose this plugin to
+    // load webpack chunks instead by overriding the default request behavior
+    // it's easier to use webpack in our current CRA to dynamically import a JSON with the translations
+    // than to update and serve a static folder with JSON files on the CDN with cache invalidation
+    try {
+      const [lng] = url.split('|');
+
+      // this mocks the HTTP fetch plugin behavior so it works with the backend AJAX pattern in this XHR library
+      // https://github.com/i18next/i18next-http-backend/blob/master/lib/request.js#L56
+      loadLocaleBundle(lng).then((data) => {
+        callback(null, {
+          data: JSON.stringify(data),
+          status: 200, // status code is required by XHR plugin to determine success or failure
+        });
+      });
+    } catch (e) {
+      console.error(e);
+      callback(null, {
+        status: 500,
+      });
+    }
+  },
+};
+
 i18n
   // detect user language
   // learn more: https://github.com/i18next/i18next-browser-languageDetector
   .use(LanguageDetector)
+  // use HTTP backend to async load translated strings
+  .use(HttpApi)
   // pass the i18n instance to react-i18next.
   .use(initReactI18next)
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
-    resources: {
-      en: { // translation is the default namespace
-        translation: en
-      },
-      es: {
-        translation: es
-      },
-      zh: {
-        translation: zh
-      }
-    },
-    fallbackLng: 'en',
+    fallbackLng: DEFAULT_LOCALE,
     debug: true,
-    whitelist: ['en', 'zh', 'es'], // available languages for browser dector to pick from
+    whitelist: availableLocales, // available languages for browser dector to pick from
     detection: langDetectorOptions,
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
-    }
+    },
+    backend: backendOptions,
   });
 
 
---
+++ src/localeBundles.js
@@ -0,0 +1,14 @@
+/**
+ * The bundles here are configured so that each locale only requires loading a single webpack chunk.
+ */
+
+const bundles = {
+  en: () => import(/* webpackChunkName: "en" */ './locales/en.json'),
+  es: () => import(/* webpackChunkName: "es" */ './locales/es.json'),
+  zh: () => import(/* webpackChunkName: "zh" */ './locales/zh.json'),
+};
+
+// generate whitelist for i18next
+export const availableLocales = Object.keys(bundles);
+
+export default bundles;
