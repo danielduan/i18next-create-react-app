bffc29867c7e1677af9e417f737d267e8aa88fb2
Daniel Duan <dduan@yahoo.com>
2020-05-26 13:24:56 -0700

    detect browser locale

--- src/i18nextInit.js
+++ src/i18nextInit.js
@@ -1,14 +1,36 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+const langDetectorOptions = {
+  // order and from where user language should be detected
+  order: ['cookie', 'localStorage', 'navigator'],
+
+  // keys or params to lookup language from
+  lookupCookie: 'locale',
+  lookupLocalStorage: 'locale',
+
+  // cache user language on
+  caches: ['localStorage', 'cookie'],
+  excludeCacheFor: ['cimode'], // languages to not persist (cookie, localStorage)
+
+  // only detect languages that are in the whitelist
+  checkWhitelist: true,
+};
 
 i18n
- .use(initReactI18next)
+  // detect user language
+  // learn more: https://github.com/i18next/i18next-browser-languageDetector
+  .use(LanguageDetector)
+  // pass the i18n instance to react-i18next.
+  .use(initReactI18next)
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
     fallbackLng: 'en',
     debug: true,
-
+    whitelist: ['en', 'zh', 'es'], // available languages for browser dector to pick from
+    detection: langDetectorOptions,
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
     }
