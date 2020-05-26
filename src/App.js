8c8742eb57098efaac680b1f52cc1517faa5f9d5
Daniel Duan <dduan@yahoo.com>
2020-05-26 16:39:11 -0700

    change language

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
 import React from 'react';
 import './App.css';
-import { useTranslation, Trans } from 'react-i18next';
+import { useTranslation, Trans, I18nextProvider } from 'react-i18next';
 
 function App() {
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
+
+  const toggleLang = () => {
+    // overly simplistic way of toggling between 3 languages
+    switch(i18n.language) {
+      case 'en':
+        i18n.changeLanguage('es');
+        break;
+      case 'es':
+        i18n.changeLanguage('zh');
+        break;
+      default:
+        i18n.changeLanguage('en');
+        break;
+    }
+  }
 
   return (
-    <div className="App">
-      <h1>
-        <Trans i18nKey="header.welcome">Welcome!</Trans>
-      </h1>
-      <p>
-        {t('home.quantity_display', 'You have 2 apples.')}
-      </p>
-    </div>
+    // language change only changes the current component and does not propagate
+    // across the app without this React context provider at the root of the app
+    <I18nextProvider i18n={i18n}>
+      <div className="App">
+        <h1>
+          <Trans i18nKey="header.welcome">Welcome!</Trans>
+        </h1>
+        <p>
+          {t('home.quantity_display', 'You have 2 apples.')}
+        </p>
+        <button onClick={toggleLang}>???</button>
+      </div>
+    </I18nextProvider>
   );
 }
 
